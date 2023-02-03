176958ea5350199635b70b682043be7e3bc9cb8c
stojo <79710988+dreadanaught@users.noreply.github.com>
2023-02-03 21:18:05 +0000

    Created parser class

---
+++ parser.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class parser {
+
+	//Converts strings from standard algebraic notation to Reverse Polish Notation
+	//Really needs to use stacks for this instead of ArrayLists, but I can't be bothered to learn how to do that right now.
+	public static ArrayList<String> shunt(String input) {
+		ArrayList<String> output = new ArrayList<>();
+		ArrayList<String> operatorStack = new ArrayList<>();
+
+		for (int i = 0; i < input.length(); i++) {
+			
+		}
+		
+		
+		
+		return output;
+	}
+}
